5e2dd0b68ee8dc328813b062b0f4fa785395ee0b
rajanikumar <rajanikanazariya@outlook.com>
2017-11-30 16:08:08 +0530

    This is my first commit

---
+++ download_app.php
@@ -0,0 +1,32 @@
+<section class="banner">
+	<div class="clearfix"></div>
+
+		<section class="section">
+			<section class="container mid-section  inner-container pt-5">
+				
+				<div class="section-right col-md-12 col-sm-3 col-xs-3 pt-10 pl-20 pr-20 mt-15">
+					
+	  				<div class="clearfix">&nbsp;</div>
+	  				<div class="wrapper app-wrapper mb-15">
+	  					<figure class="figure mt-15">
+	  						<div class="figure-img">
+	  							<img class="img-responsive" src="<?php echo SITE_URL;?>/assets/images/app_familybg.jpg">
+	  						</div>
+	  						<figcaption class="figure-text mt-50">
+	  							<span class="heading wrapper text-center">
+	  								<span id="lng_bpart"> Be a part of our family </span>&amp;<br> <span id="lng_pckt"> get everything in your pocket</span>
+	  							</span>
+	  							<span class="subheading wrapper text-center" id="lng_dwnl">Download the <?php echo SITE_TITLE; ?> App</span>
+	  							<div class="wrapper app-store text-center">
+	  								<a href="<?php echo $main->site_data['ios_url']; ?>" taret="_new" title="Local Search on Mobile from <?php echo SITE_TITLE; ?>"><span class="footer_sprite pstore-icon"></span></a>
+	  								<a href="<?php echo $main->site_data['android_url']; ?>" target="_new" title="Local Search on Mobile from <?php echo SITE_TITLE; ?>"><span class="footer_sprite istore-icon"></span></a>
+	  							</div>
+	  						</figcaption>
+	  					</figure>
+	  				</div>
+				</div>
+			</section>
+		</section>
+</section>
+<div class="clearfix"></div>
+<?php @include("include/footer.php");?>
